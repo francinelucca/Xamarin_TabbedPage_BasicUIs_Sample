9987aa592da2f52b67b212d8d9239f388000005b
francinelucca <francinelucca@hotmail.com>
2020-02-09 19:34:19 -0400

    Merge

---
+++ Xamarin_TabbedPage_Sample/Xamarin_TabbedPage_Sample/BookmarkPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin_TabbedPage_Sample
+{
+    public partial class BookmarkPage : ContentPage
+    {
+        public BookmarkPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
